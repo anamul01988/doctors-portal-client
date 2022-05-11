ece6f57cece9796d4e4839168879a2288da1fdfa
Anamul haque  shanto <anamulhaque0198877@gmail.com>
2022-05-11 21:55:56 +0600

    few seciton added

--- src/Pages/Home/home.js
+++ src/Pages/Home/home.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import Banner from './Banner';
+import Footer from './Footer';
 import Info from './Info';
+import MakeAppointment from './MakeAppointment';
 import Services from './Services';
+import Testimonials from './Testimonials';
 
 const Home = () => {
     return (
@@ -9,6 +12,9 @@ const Home = () => {
             <Banner/>
             <Info/>
             <Services></Services>
+            <MakeAppointment/>
+            <Testimonials/>
+            <Footer/>
         </div>
     );
 };

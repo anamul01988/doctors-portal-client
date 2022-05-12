690412644d5f8a3e13c165ae0c6b1a16ee126cee
Anamul haque  shanto <anamulhaque0198877@gmail.com>
2022-05-13 00:13:24 +0600

    login validation with form done

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,20 +1,92 @@
 import React from "react";
 import auth from "../../firebase.init";
-import { useSignInWithGoogle} from 'react-firebase-hooks/auth';
+import { useSignInWithGoogle } from "react-firebase-hooks/auth";
+import { useForm } from "react-hook-form";
 const Login = () => {
-    const [signInWithGoogle, user, loading, error] = useSignInWithGoogle(auth);
-    if(user){
-        console.log(user)
-    }
+  const [signInWithGoogle, user, loading, error] = useSignInWithGoogle(auth);
+  const {
+    register,
+    formState: { errors },
+    handleSubmit,
+  } = useForm();
+  if (user) {
+    console.log(user);
+  }
+  const onSubmit = (data) => {
+    console.log(data);
+  };
   return (
     <div className="flex h-screen justify-center items-center">
       <div className="card w-96 bg-base-100 shadow-xl">
         <div className="card-body">
           <h2 className="text-center text-2xl font-bold">Login</h2>
+          <form onSubmit={handleSubmit(onSubmit)}>
+            <div class="form-control w-full max-w-xs">
+              <label class="label">
+                <span class="label-text">Email</span>
+              </label>
+              <input
+                type="email"
+                placeholder="email@gmail.com"
+                class="input input-bordered w-full max-w-xs"
+                {...register("email", {
+                    required: {
+                        value: true,
+                        message: 'Email is Required'
+                     },
+                    pattern: {
+                        value: /[a-z0-9]+@[a-z]+\.[a-z]{2,3}/,
+                        message: 'Provide a valid Email'
+                    }
+                  })}
+              />
+              <label class="label">
+              {errors.email?.type === 'required' &&  <span class="label-text-alt text-red-500">{errors.email.message}</span>}
+              {errors.email?.type === 'pattern' &&  <span class="label-text-alt text-red-500">{errors.email.message}</span>}
+              </label>
+            </div>
+
+            <div class="form-control w-full max-w-xs">
+              <label class="label">
+                <span class="label-text">Password</span>
+              </label>
+              <input
+                type="password"
+                placeholder="type your password"
+                class="input input-bordered w-full max-w-xs"
+                {...register("password", {
+                    required: {
+                        value: true,
+                        message: 'Password is Required'
+                     },
+                    minLength: {
+                        value: 6,
+                        message: 'Must be 6 characters or longer'
+                    }
+                  })}
+              />
+              <label class="label">
+              {errors.password?.type === 'required' &&  <span class="label-text-alt text-red-500">{errors.password.message}</span>}
+              {errors.password?.type === 'minLength' &&  <span class="label-text-alt text-red-500">{errors.password.message}</span>}
+              </label>
+            </div>
+
+            {/* <input {...register("firstName", { required: true })} />
+            {errors.firstName?.type === "required" && "First name is required"}
+
+            <input {...register("lastName", { required: true })} />
+            {errors.lastName && "Last name is required"} */}
+
+            <input className="btn w-full max-w-xs text-white" type="submit" value="Login" />
+          </form>
           <div className="divider">OR</div>
-          <button onClick={() => signInWithGoogle()} className="btn btn-outline">Continue With Google</button>
+          <button
+            onClick={() => signInWithGoogle()}
+            className="btn btn-outline"
+          >
+            Continue With Google
+          </button>
         </div>
-     
       </div>
     </div>
   );

426c762dd6d4e5d6feede6cf7e8b85417a44810a
Anamul haque  shanto <anamulhaque0198877@gmail.com>
2022-05-15 00:21:12 +0600

    appointment background added

--- src/Pages/Appointment/AppointmentBanner.js
+++ src/Pages/Appointment/AppointmentBanner.js
@@ -2,10 +2,14 @@
 import React from 'react';
 import { DayPicker } from 'react-day-picker';
 import chair from '../../assets/images/chair.png';
+import bg from '../../assets/images/bg.png';
 const AppointmentBanner = ({date, setDate}) => {
     // const [date, setDate] = React.useState(new Date());
     return (
-        <div className="hero min-h-screen ">
+        <div  style={{
+          background: `url(${bg})`,
+          backgroundSize: 'cover'
+      }} className="hero min-h-screen ">
         <div className="hero-content flex-col lg:flex-row-reverse">
           <img src={chair} alt="dentist_chair" className="max-w-sm rounded-lg shadow-2xl" />
           <div >
